58801eb72e67acc76200930f597d3cb20ae664b1
Mriganka Sarma <139118018+mriganka528@users.noreply.github.com>
2024-07-23 22:57:08 +0530

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -6,7 +6,7 @@ function About() {
     return (
         <div className='px-10  w-screen flex justify-evenly  items-center pt-48'>
             <div className=' flex flex-col justify-center  '>
-                <h1 className="heading  ">Hello I'm,</h1>
+                <h1 className="heading  ">Hello I&apos;m&rsquo; </h1>
                 <h1 className='Type'> Mriganka Sarma |</h1>
                 <p className='text-left text-gray-400 text-wrap antialiased text-lg max-w-[50rem]'>Hello myself, Mriganka Sarma: Versatile full-stack developer proficient in front-end and back-end technologies.</p>
                 <div className='flex pt-3 space-x-5'>
